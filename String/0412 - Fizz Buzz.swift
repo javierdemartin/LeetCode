7de57d35d68cadd9f04291f22ca4a1017057ffbe
Javier de Martín Gil <javierdemartin@me.com>
2020-12-29 17:39:51 +0100

    Added 0412 - Fizz Buzz

---
+++ String/0412 - Fizz Buzz.swift
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/fizz-buzz/submissions/
+
+import Foundation
+
+class Solution {
+    func fizzBuzz(_ n: Int) -> [String] {
+     
+        var result = [String]()
+        
+        for i in 1...n {
+            if i % 5 == 0 && i % 3 == 0 {
+                result.append("FizzBuzz")
+            } else if i % 5 == 0 {
+                result.append("Buzz")
+            } else if i % 3 == 0 {
+                result.append("Fizz")
+            }
+            
+            else {
+                result.append("\(i)")
+            }
+        }
+        
+        return result
+    }
+}
+
