364917ed37a32de17533c4b36148eeb5cd03f23c
Javier de Martín Gil <javierdemartin@me.com>
2021-01-10 11:28:18 +0100

    Added 0263 - Ugly Number

---
+++ 0263 - Ugly Number.swift
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/ugly-numer/
+
+import Foundation
+
+class Solution {
+    func isUgly(_ num: Int) -> Bool {
+        
+        if num < 1 { return false }
+        
+        var num = num
+        
+        while num != 1 {
+            if num % 2 == 0 {
+                num /= 2
+            } else if num % 3 == 0 {
+                num /= 3
+            } else if num % 5 == 0 {
+                num /= 5
+            } else {
+                return false
+            }
+        }
+        
+        return true
+    }
+}
+
+let s = Solution()
+s.isUgly(8)
+
