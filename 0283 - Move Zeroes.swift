9e6d67e70d86601be8635abdfc5ff235d53e1b1e
Javier de Martín Gil <javierdemartin@me.com>
2021-01-13 16:37:46 +0100

    Added 0283 - Move Zeroes

---
+++ 0283 - Move Zeroes.swift
@@ -0,0 +1,60 @@
+ // https://leetcode.com/problems/intersection-of-two-arrays-ii/
+ 
+ import Foundation
+ 
+ class Solution {
+    
+    /**
+     Fastest one that does in-place substitutions by swapping the elements
+     
+     - time complexity: O(N)
+     - space complexity: O(1)
+     */
+    func moveZeroes(_ nums: inout [Int]) {
+        
+        var latestNonZeroAt = 0
+        
+        for i in 0..<nums.count {
+            if nums[i] != 0 {
+                nums[latestNonZeroAt] = nums[i]
+                if latestNonZeroAt != i { nums[i] = 0 }
+                latestNonZeroAt += 1
+            }
+        }
+    }
+    
+    /**
+     Suboptimal solution.
+     
+     - time complexity: O(N)
+     - space complexity: O(N)
+     */
+    func moveZeroes1(_ nums: inout [Int]) {
+        
+        var zeros = 0
+        var nonZeroes = [Int]()
+        
+        for i in 0..<nums.count {
+            if nums[i] == 0 {
+                zeros += 1
+            } else {
+                nonZeroes.append(nums[i])
+            }
+        }
+        
+        if zeros == nums.count { return }
+        
+        for i in 0..<nums.count {
+            nums[i] = 0
+        }
+        
+        for i in 0..<nonZeroes.count {
+            nums[i] = nonZeroes[i]
+        }
+    }
+ }
+ 
+ var a = [1,1,0,3,1,0,2]
+ 
+ Solution().moveZeroes(&a)
+
