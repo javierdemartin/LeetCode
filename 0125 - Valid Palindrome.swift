6e0d2ab094e14db55bd85c502922a5faad0ac28b
Javier de Martín Gil <javierdemartin@me.com>
2021-01-13 22:11:17 +0100

    Updated 0125 - Valid Palindrome

--- 0125 - Valid Palindrome.swift
+++ 0125 - Valid Palindrome.swift
@@ -3,7 +3,37 @@
 import Foundation
 
 class Solution {
+    
     func isPalindrome(_ s: String) -> Bool {
+        
+        var i = 0, j = s.count - 1
+        var s = Array(s)
+        
+        while i < j {
+            
+            // If left-most pointer is not an alphanumeric character skip it
+            if !s[i].isLetter && !s[i].isNumber {
+                i += 1
+            }
+            // If right-most pointer is not an alphanumeric character skip it
+            else if !s[j].isLetter && !s[j].isNumber {
+                j -= 1
+            }
+            // Both characters are a match, keep iterating
+            else if String(s[i]).lowercased() == String(s[j]).lowercased() {
+                i += 1; j -= 1;
+            }
+            // Not satisfying any of the above conditions means that this string is
+            else {
+                return false
+
+            }
+        }
+        
+        return true
+    }
+    
+    func isPalindrome1(_ s: String) -> Bool {
         
         // 1. Remove non-alphanumeric characters
         let pattern = "[^A-Za-z0-9]+"
@@ -23,4 +53,6 @@ class Solution {
 }
 
 let a = Solution()
-a.isPalindrome("A man, a plan, a canal: Panama")
+a.isPalindrome("cat a e ")
+//a.isPalindrome("A man, a plan, a canal: Panama")
+
