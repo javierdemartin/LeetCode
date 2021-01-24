07784ae08ddf7d6fee9bdbbe1c0de4515785b370
Javier de Martín Gil <javierdemartin@me.com>
2021-01-24 16:22:21 +0100

    Added 0142 - Linked List Cycle II

---
+++ 0142 - Linked List Cycle II.swift
@@ -0,0 +1,49 @@
+// https://leetcode.com/problems/linked-list-cycle-ii/
+
+import Foundation
+
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init(_ val: Int) {
+        self.val = val
+        self.next = nil
+    }
+}
+
+/**
+ Can't use a simple dictionary to store the current head's value and number of times visited because `ListNode` doesn't conform to hashable.
+ 
+ By using a set that contains each node's pointer in memory we can check if a cycle exists as when a new node's pointer is not inserted into the set because it's already there.
+ */
+class Solution {
+    func detectCycle(_ head: ListNode?) -> ListNode? {
+        
+        var visits: Set<UnsafeMutableRawPointer> = []
+        
+        var head = head
+        
+        while head != nil {
+            
+            if !visits.insert(Unmanaged.passUnretained(head!).toOpaque()).inserted {
+                return head
+            }
+            
+            head = head?.next
+        }
+        
+        return nil
+    }
+}
+
+let l1 = ListNode(3)
+let l2 = ListNode(2)
+let l3 = ListNode(0)
+let l4 = ListNode(-4)
+
+l1.next = l2
+l2.next = l3
+l3.next = l4
+l4.next = l2
+
+Solution().detectCycle(l1)
