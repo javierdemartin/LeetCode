f101ff2832b2148a87fa0fe38901f8ff400b3f3b
Javier de Martín Gil <javierdemartin@me.com>
2021-01-28 17:45:34 +0100

    Updated 0155 - Min Stack

--- 0155 - Min Stack.swift
+++ 0155 - Min Stack.swift
@@ -1,65 +1,51 @@
-// https://leetcode.com/problems/min-stack/
+// https://leetcode.com/problems/min-stack/submissions/
 
 import Foundation
 
+/**
+ Using stacks this operation works in O(1) time by keeping a timeline of the minimum values as they are being added to the stack.
+ 
+ Space complexity: O(N)
+ Time complexity: O(1)
+ */
+
 class MinStack {
     
-    var minValue: Int {
-        didSet {
-            print("> new min \(minValue)")
-        }
-    }
-    var stack: [Int]
+    var minValue: [Int] = []
+    var stack: [Int] = []
 
     /** initialize your data structure here. */
     init() {
-        minValue = Int.max
-        stack = []
+        
     }
     
     func push(_ x: Int) {
-        minValue = min(minValue, x)
+        
+        if stack.isEmpty {
+            minValue.append(x)
+        } else {
+            minValue.append(min(x, minValue[minValue.count - 1]))
+        }
+        
         stack.append(x)
     }
     
     func pop() {
+        
         stack.removeLast()
         
-        minValue = Int.max
-        
-        /// Find the possible new minimum value
-        for i in stack {
-            if i < minValue {
-                minValue = i
-                
-            }
-        }
+        minValue.removeLast()
     }
     
     func top() -> Int {
-        return stack.last!
+        return stack.isEmpty ? -1 : stack.last!
     }
     
     func getMin() -> Int {
-        return minValue
+        return stack.isEmpty ? -1 : minValue.last!
     }
 }
 
-/**
- * Your MinStack object will be instantiated and called as such:
- * let obj = MinStack()
- * obj.push(x)
- * obj.pop()
- * let ret_3: Int = obj.top()
- * let ret_4: Int = obj.getMin()
- */
-
 let obj = MinStack()
-obj.push(-2)
-obj.push(0)
-obj.push(-3)
-obj.getMin()
-obj.pop()
-obj.top()
-obj.getMin()
+obj.push(5)
 
