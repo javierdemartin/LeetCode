c2a8ed6d27b338d2b3929a224a501faee1f3ad07
Javier de Martín Gil <javierdemartin@me.com>
2021-02-03 22:39:12 +0100

    Updated 0013 - Roman to Integer

--- 0013 - Roman to Integer.swift
+++ 0013 - Roman to Integer.swift
@@ -1,23 +1,43 @@
+/// https://leetcode.com/problems/roman-to-integer/
+
+import Foundation
+
+/**
+ Time complexity: O(1) There is a finite set of roman numerals available
+ Space complexity: O(1) Only using a constant number of variables that don't grow when the input changes
+ */
 class Solution {
+    
+    private var map: [Character: Int] = ["I": 1,"V": 5,"X": 10,"L": 50,"C": 100,"D": 500,"M": 1000]
+    
     func romanToInt(_ s: String) -> Int {
-        let translator: [Character: Int] = ["I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000]
         
-        var romans = Array(s)
-        var integer = 0
-        var previousInteger = 0
+        let s = Array(s)
         
-        for character in romans {
-            if let val = translator[character] {
-                if val <= previousInteger {
-                    integer += val
-                } else {
-                    integer += (val - 2 * previousInteger)
-                }
-                
-                previousInteger = val
+        /// Will progressively build up the solution
+        var result = 0
+        /// Pointer which advances to the right depending on the case found
+        var i = 0
+        
+        while i < s.count {
+            
+            /// Substractive case, for example `IV`.
+            if i + 1 < s.count && map[s[i]]! < map[s[i+1]]! {
+                /// Add the difference between the current and the next value
+                result += map[s[i+1]]! - map[s[i]]!
+                /// Advance two steps
+                i += 2
+            }
+            /// Not the substractive case
+            else {
+                result += map[s[i]]!
+                i += 1
             }
         }
         
-        return integer
+        return result
     }
 }
+
+Solution().romanToInt("XVII")
+
