98721546ce4ce7b5adf23b018dfe64a8640afe6e
Javier de Martín Gil <javierdemartin@me.com>
2021-02-03 11:20:36 +0100

    Updated 0234 - Palindrome Linked List

--- 0234 - Palindrome Linked List.swift
+++ 0234 - Palindrome Linked List.swift
@@ -1,4 +1,4 @@
-// https://leetcode.com/problems/palindrome-linked-list/
+/// https://leetcode.com/problems/longest-palindromic-substring/
 
 import Foundation
 
@@ -10,31 +10,45 @@ public class ListNode {
     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
 }
 
+
+/**
+ Recursive solution by traversing the list with two pointers.
+ Algorithm consists of getting to the end of the list, stopping the recursion there and when coming back
+ progressively comparing a pointer pointing to the head if the values match (palindrome) if both match
+ move forwards the `frontPointer`. The tail pointer will be moving automatically as the runtime stack is
+ being resolved.
+ 
+ Time complexity: O(N)
+ Space complexity: O(N) due to the added runtime stack when using recursion.
+ */
 class Solution {
-    func isPalindrome(_ head: ListNode?) -> Bool {
-        var values = [Int]()
+    
+    private var frontPointer: ListNode? = nil
+    
+    private func recurse(_ head: ListNode?) -> Bool {
         
-        var initial = head
-        
-        while initial != nil {
-            values.append(initial!.val)
-            initial = initial?.next
-        }
-        
-        for i in 0..<Int(values.count/2) {
-            print("\(values[i]) \(values[values.count - 1 - i])")
-            if values[i] != values[values.count - 1 - i] {
-                return false
-            }
-            
+        if head != nil {
+            if !recurse(head?.next) { return false }
+            /// This makes sense because `frontPointer` will traverse the list from the beginning
+            /// when the recursion has ended so `head` will be at the end
+            print("Head \(head!.val) front \(frontPointer!.val)")
+            if head!.val != frontPointer!.val { return false }
+            /// Moving this pointer forward me manage to move the `frontPointer` to the right
+            /// as the same time the `head` is moving backwards from the right side
+            frontPointer = frontPointer?.next
         }
         
         return true
     }
+    
+    func isPalindrome(_ head: ListNode?) -> Bool {
+        
+        frontPointer = head
+        
+        return recurse(head)
+    }
 }
 
-let tree = ListNode(1, ListNode(2, ListNode(2, ListNode(1))))
-
-let s = Solution()
-let returned = s.isPalindrome(tree)
+var head = ListNode(1, ListNode(3, ListNode(3, ListNode(1))))
+Solution().isPalindrome(head)
 
