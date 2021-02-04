966fcdf599b7f980848b58a9b6c97da6dac622ef
Javier de Martín Gil <javierdemartin@me.com>
2021-02-04 12:53:03 +0100

    Updated 1094 - Car Pooling

--- 1094 - Car Pooling.swift
+++ 1094 - Car Pooling.swift
@@ -1,46 +1,38 @@
-// https://leetcode.com/problems/car-pooling/
-
-import Foundation
+/// https://leetcode.com/problems/car-pooling/submissions/
 
 class Solution {
+    
+    /**
+     Create a timeline with the overall number of people that leave the car at a time `i`.
+     
+     Time complexity: O(min(N, 1.001)), where `N` is the biggest drop time
+     Space complexity: O(1)
+     */
     func carPooling(_ trips: [[Int]], _ capacity: Int) -> Bool {
-
-        var ups: [Int: Int] = [:]
-        var downs: [Int: Int] = [:]
-        var peopleIn = 0
-        var minRange = Int.max
-        var maxRange = Int.min
-
+        
+        let maxTime = min(1001, trips.compactMap({ $0[2] }).max()! + 1)
+        
+        var timeline: [Int] = Array(repeating: 0, count: maxTime)
+        
         for trip in trips {
-
-            ups[trip[1], default: 0] += trip[0]
-            downs[trip[2], default: 0] += trip[0]
-
-            if minRange > trip[1] {
-                minRange = trip[1]
-            }
-
-            if trip[2] > maxRange {
-                maxRange = trip[2]
-            }
+            /// At time `trip[1]` add up the people that go into the car
+            timeline[trip[1]] += trip[0]
+            /// At time `trip[2]` substract the people that leave the car
+            timeline[trip[2]] -= trip[0]
         }
-
-        for i in minRange...maxRange {
-
-            if let ins = ups[i] {
-                peopleIn += ins
-            }
-
-            if let outs = downs[i] {
-                peopleIn -= outs
-            }
-
-            if peopleIn > capacity { return false }
+        
+        var currentCapacity = 0
+        
+        for n in timeline {
+            currentCapacity += n
+            
+            if currentCapacity > capacity { return false }
         }
-
+        
         return true
     }
 }
 
-Solution().carPooling([[2,1,5],[3,3,7]], 4) // false
-Solution().carPooling([[5,4,7],[7,4,8],[4,1,8]], 17) // true
+//Solution().carPooling([[2,1,5],[3,3,7]], 4) // false
+Solution().carPooling([[2,1,5],[3,3,7]], 5) // true
+
