c4268235491d055e9c96bafa5b65400566644262
Javier de Martín Gil <javierdemartin@me.com>
2021-02-04 13:42:11 +0100

    Added 0244 - Shortest Word Distance II

---
+++ 0244 - Shortest Word Distance II.swift
@@ -0,0 +1,48 @@
+/// https://leetcode.com/problems/shortest-word-distance-ii/
+
+import Foundation
+
+class WordDistance {
+
+    /// For any given `key` store all the indexes in which the `key` appears on the given input
+    var wordsD: [String: [Int]] = [:]
+    
+    /**
+     Time complexity: O(N)
+     Space complexity: O(N)
+     */
+    init(_ words: [String]) {
+        
+        for (i, w) in words.enumerated() {
+            if wordsD[w] == nil {
+                wordsD[w] = [i]
+            } else {
+                wordsD[w]!.append(i)
+            }
+        }
+    }
+    
+    /**
+     Time complexity: O(N^2)
+     Space complexity: O(1)
+     */
+    func shortest(_ word1: String, _ word2: String) -> Int {
+        
+        let pos1 = wordsD[word1]!
+        let pos2 = wordsD[word2]!
+        
+        var minD = Int.max
+        
+        for p1 in pos1 {
+            for p2 in pos2 {
+                minD = min(minD, abs(p1 - p2))
+            }
+        }
+        
+        return minD
+    }
+}
+
+WordDistance(["practice", "makes", "perfect", "coding", "makes"]).shortest("coding", "practice")
+
+
