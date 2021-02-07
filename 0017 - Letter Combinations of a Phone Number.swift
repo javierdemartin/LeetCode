7a98fcb9013534e81a54b27e9455a492597aa6f8
Javier de Martín Gil <javierdemartin@me.com>
2021-02-07 20:35:04 +0100

    Updated 0017 Letter Combinations of a Phone Number

--- 0017 - Letter Combinations of a Phone Number.swift
+++ 0017 - Letter Combinations of a Phone Number.swift
@@ -1,52 +1,50 @@
-/// https://leetcode.com/problems/letter-combinations-of-a-phone-number/
+/// https://leetcode.com/problems/permutations/
 
 import Foundation
 
 class Solution {
+    
     func letterCombinations(_ digits: String) -> [String] {
-        
-        if digits.isEmpty || digits == "1" { return [] }
-        
+       
+				if digits.isEmpty { return [] }
+ 
         let map: [Character: String] = ["2": "abc", "3": "def", "4": "ghi", "5": "jkl", "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"]
         
-        /// Array of Strings containing the letter combinations to assemble
-        var str = [String]()
+        /// Given the `digits` pressed on the keypad translate the possible combinations to the represented characters on the keypad
+        let keyPresses: [String] = digits.map { map[$0]! }
         
-        /// Array of Strings containing the combinations
-        var combinations = [String]()
-        /// Current combinations
-        var currentCombinations = ""
+        /// Store the solution of the possible combinations generated, each String will have the same length as the digits pressed.
+        /// If the input is of length `2` each generated combination will be of the same length.
+        var combinations: [String] = []
         
-        str = digits.compactMap({ map[$0] })
+        var current: String = ""
         
-        print(str)
-        backtrack(str, 0, &currentCombinations, &combinations)
+        backtrack(keyPresses, 0, &current, &combinations)
         
         return combinations
+        
     }
     
-    func backtrack(_ str: [String], _ pos: Int, _ currentCombinations: inout String, _ combinations: inout [String]) {
+    func backtrack(_ keyPresses: [String], _ pos: Int, _ current: inout String, _ result: inout [String]) {
         
-        /// No more digits to check so the current combination is done
-        if currentCombinations.count == str.count {
-            /// Append the value to the final solution
-            combinations.append(currentCombinations)
+        /// Backtrack condition to stop the recursion. Whenever the built String reaches the same length as the
+        /// keys pressed on the keypad the combination of chracters is appended to the result.
+        if current.count == keyPresses.count {
+            result.append(current)
             return
         }
         
-        /// There are digits still to check
-        /// Iterate over all the letters which map the next available digit
-        for c in str[pos] {
+        /// This will iterate up to the lenght of the keys pressed.
+        for chars in keyPresses[pos] {
+            
             /// Append the current letter to the combination
-            currentCombinations.append(c)
-            /// Advance one digit to keep on building the string
-            backtrack(str, pos + 1, &currentCombinations, &combinations)
-            /// This is the actual backtracking
-            currentCombinations.removeLast()
+            current.append(chars)
+            /// Explore new paths
+            backtrack(keyPresses, pos + 1, &current, &result)
+            /// Actual backtrack, remove the current letter so new paths can be explored
+            current.removeLast()
         }
+        
     }
-    
 }
-
-print(Solution().letterCombinations("23"))
 
