3337f42912fab1ce120bbcdc29a1ca86249216ff
Javier de Martín Gil <javierdemartin@me.com>
2021-02-15 15:01:33 +0100

    Update 0118 - Pascal's Triangle

--- 0118 - Pascal's Triangle.swift
+++ 0118 - Pascal's Triangle.swift
@@ -1,31 +1,38 @@
-// https://leetcode.com/problems/pascals-triangle/submissions/
+// https://leetcode.com/problems/pascals-triangle/
 
 import Foundation
 
+/**
+ In my head thinking of a triangle with arrays is difficult. The Pascal triangle can be seen like this too,
+ 
+ 1
+ 1 1
+ 1 2 1
+ 1 2 2 1
+ 
+ Start with a pre-computed solution containing `[[1],[1,1]]`. On each row the first and last values will be inserted manually.
+ The calculation of each value is: `currentRow  = previousRow[i] + previousRow[i-1]`. Bear in mind that all of this is
+ 1-indexed.
+ */
 class Solution {
     func generate(_ numRows: Int) -> [[Int]] {
         
-        var solution: [[Int]] = []
+        if numRows == 0 { return [] }
+        if numRows == 1 { return [[1]] }
+        if numRows == 2 { return [[1],[1,1]]}
+     
+        var solution: [[Int]] = [[1],[1,1]]
         
-        if numRows == 0 { return solution}
-
-        solution = [[1]]
-        
-        for level in 1..<numRows {
+        for i in 3...numRows {
             solution.append([1])
             
-            let prevLevel = solution[level-1]
-            
-            for i in 1..<level {
-                solution[level].append(prevLevel[i-1] + prevLevel[i])
+            for j in 1..<i-1 {
+                solution[i-1].append(solution[i-2][j-1] + solution[i-2][j])
             }
             
-            solution[level].append(1)
+            solution[i-1].append(1)
         }
         
         return solution
     }
 }
-
-Solution().generate(3)
-
