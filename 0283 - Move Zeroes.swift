9b85f54799e74b9a28d73b8d38ae45eda5db5d2f
Javier de Martín Gil <javierdemartin@me.com>
2021-02-22 13:43:03 +0100

    Updated 0283 - Move Zeroes

--- 0283 - Move Zeroes.swift
+++ 0283 - Move Zeroes.swift
@@ -2,56 +2,27 @@
  
  import Foundation
  
+ /**
+  Time complexity: O(N)
+  Space complexity: O(1)
+  */
  class Solution {
-    
-    /**
-     Fastest one that does in-place substitutions by swapping the elements
-     
-     - time complexity: O(N)
-     - space complexity: O(1)
-     */
-    func moveZeroes(_ nums: inout [Int]) {
-        
-        var latestNonZeroAt = 0
-        
-        for i in 0..<nums.count {
-            if nums[i] != 0 {
-                nums[latestNonZeroAt] = nums[i]
-                if latestNonZeroAt != i { nums[i] = 0 }
-                latestNonZeroAt += 1
-            }
-        }
-    }
-    
-    /**
-     Suboptimal solution.
-     
-     - time complexity: O(N)
-     - space complexity: O(N)
-     */
-    func moveZeroes1(_ nums: inout [Int]) {
-        
-        var zeros = 0
-        var nonZeroes = [Int]()
-        
-        for i in 0..<nums.count {
-            if nums[i] == 0 {
-                zeros += 1
-            } else {
-                nonZeroes.append(nums[i])
-            }
-        }
-        
-        if zeros == nums.count { return }
-        
-        for i in 0..<nums.count {
-            nums[i] = 0
-        }
-        
-        for i in 0..<nonZeroes.count {
-            nums[i] = nonZeroes[i]
-        }
-    }
+     func moveZeroes(_ nums: inout [Int]) {
+         
+         var c = 0
+         
+         for i in 0..<nums.count {
+             if nums[i] != 0 {
+                 
+                 let temp = nums[i]
+                 nums[i] = nums[c]
+                 nums[c] = temp
+                 
+                 c+=1
+                 
+             }
+         }
+     }
  }
  
  var a = [1,1,0,3,1,0,2]
